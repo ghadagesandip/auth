f0194e99c52bdb8d1cfa01f9557f54444a630a7c
Sandip Ghadge <ghadagesandip@gmail.com>
2022-01-14 10:49:02 +0530

    update emv

--- src/modules/auth/__mocks__/auth.lib.ts
+++ src/modules/auth/__mocks__/auth.lib.ts
@@ -1,5 +1,6 @@
 export default class AuthLib {
   public async loginUserAndCreateToken(email: string, password: string) {
+    console.log('mock called');
     return jest.fn().mockReturnValue({ userData: {}, token: 'ad' });
   }
 }
